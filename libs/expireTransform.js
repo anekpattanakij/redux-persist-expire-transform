1bfe2058abcca7dc21ba2488eb849e3484f54137
DESKTOP-J7JJU4M\a.anekpattanakij <a.anekpattanakij@gmail.com>
2019-04-14 11:43:39 +0700

    update

--- libs/expireTransform.js
+++ libs/expireTransform.js
@@ -1,24 +1,28 @@
-const { createTransform } = require('redux-persist');
+const { createTransform } = require("redux-persist");
 
-const ExpiredTransform = (expireInMinutes,whitelist) => createTransform(
-  inboundState => {
-    if (inboundState.expirePersistTime) {
-      var currentDate = new Date();
-      currentDate.setMinutes( currentDate.getMinutes() + expireInMinutes );
-      return { ...inboundState };
-    } else {
-      return { ...inboundState, expirePersistTime: currentDate };
-    }
-  },
-  outboundState => {
-    if (outboundState.expirePersistTime && outboundState.expirePersistTime < new Date()) {
-      return {};
-    } else {
-      return { ...outboundState };
-    }
-  },
-  // define which reducers this transform gets called for.
-  { whitelist: whitelist },
-);
+const ExpiredTransform = (expireInMinutes, whitelist) =>
+  createTransform(
+    inboundState => {
+      if (inboundState.expirePersistTime) {
+        return { ...inboundState };
+      } else {
+        var currentDate = new Date();
+        currentDate.setMinutes(currentDate.getMinutes() + expireInMinutes);
+        return { ...inboundState, expirePersistTime: currentDate };
+      }
+    },
+    outboundState => {
+      if (
+        outboundState.expirePersistTime &&
+        outboundState.expirePersistTime < new Date()
+      ) {
+        return {};
+      } else {
+        return { ...outboundState };
+      }
+    },
+    // define which reducers this transform gets called for.
+    { whitelist }
+  );
 
 module.exports = ExpiredTransform;
